229a4c05621896603615d5eb6a3191aaf1ed88ee
willmcb94 <williammcburney@Williams-Air.fritz.box>
2022-03-21 12:47:04 +0000

    add onChange to email/password

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,19 +1,43 @@
-import React from 'react'
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
+import { Button, KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native'
+import {getAuth, signInWithEmailAndPassword} from "firebase/auth"
+import { set } from 'firebase/database'
 
 const LoginScreen = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
         <KeyboardAvoidingView
             style={styles.container}
             behavior="padding">
-            <Text>Login</Text>
-            <View>
-                <TextInput placeholder="email"/>
+            
+            <View style={styles.inputs}>
+                <TextInput defaultValue={email}placeholder="email" onChangeText={(newText)=>{setEmail(newText)}} />
+                <TextInput defaultValue={password} secureTextEntry placeholder="password" onChangeText={(newText) => { setPassword(newText) }} />
+                {console.log(email, password)}
             </View>
+            
+            <Button title="Login" color="white" accessibilityLabel="login-button" onPress={() => {}}/>
+            
+                <Text>If you dont have an account, sign up here</Text>
+            
         </KeyboardAvoidingView>
     )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#718355"
+    },
+    inputs: {
+        backgroundColor: "white",
+        width: "60%",
+        padding: 5,
+        borderRadius: 5
+    }
+})

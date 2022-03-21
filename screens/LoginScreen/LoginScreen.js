de497b0f81542ec8481d62e69c4fd47eb723e441
Brynster <b.abendstern@googlemail.com>
2022-03-21 15:29:03 +0000

    sort styling, add react-native-button

--- screens/LoginScreen/LoginScreen.js
+++ screens/LoginScreen/LoginScreen.js
@@ -1,43 +1,65 @@
-import React, { useState } from 'react'
-import { Button, KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native'
-import {signInWithEmailAndPassword} from 'firebase/auth'
-import { set } from 'firebase/database'
-import { auth, database } from '../../firebase'
-import styles from "./styles";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import React, { useState } from 'react';
+import { auth, database } from '../../firebase';
+
+import Button from 'react-native-button';
+import { set } from 'firebase/database';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import styles from './styles';
 
 const LoginScreen = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const handleLogin = () => {
-        console.log(`${email}`, `${password}`)
-        signInWithEmailAndPassword(auth, email, password).then((res) => {
-    console.log(res)
-        }).catch((err) => {
-    console.log(err)
-})
-    }
+  const handleLogin = () => {
+    console.log(`${email}`, `${password}`);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    
-    return (
-        <KeyboardAvoidingView
-            style={styles.container}
-            behavior='padding'>
-            
-            <View style={styles.login_inputs_container}>
-                <TextInput style={styles.login_input} defaultValue={email} placeholder='email' onChangeText={(newText)=>{setEmail(newText)}} />
-                <TextInput style={styles.login_input} defaultValue={password}  placeholder='password' onChangeText={(newText) => { setPassword(newText) }} />
-                
-            </View>
-            
-            <Button title='Login' color='white' accessibilityLabel='login-button' onPress={handleLogin}/>
-            
-                <Text>If you dont have an account, sign up here</Text>
-            
-        </KeyboardAvoidingView>
-    )
-}
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <View style={styles.login_inputs_container}>
+        <TextInput
+          style={styles.login_input}
+          defaultValue={email}
+          placeholder="email"
+          onChangeText={(newText) => {
+            setEmail(newText);
+          }}
+        />
+        <TextInput
+          style={styles.login_input}
+          defaultValue={password}
+          placeholder="password"
+          onChangeText={(newText) => {
+            setPassword(newText);
+          }}
+        />
+      </View>
 
-export default LoginScreen
+      <Button
+        style={styles.login_button}
+        accessibilityLabel="login-button"
+        onPress={handleLogin}
+      >
+        Login
+      </Button>
 
+      <Text>If you dont have an account, sign up here</Text>
+    </KeyboardAvoidingView>
+  );
+};
 
+export default LoginScreen;
--- screens/LoginScreen/styles.js
+++ screens/LoginScreen/styles.js
@@ -1,19 +1,30 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
-export default styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#718355"
-    },
-    login_inputs_container: {
-        backgroundColor: "white",
-        width: "60%",
-        padding: 5,
-        borderRadius: 5
-    },
-    login_input: {
-        padding: 5
-    }
-})
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#718355',
+  },
+  login_inputs_container: {
+    color: 'black',
+    backgroundColor: 'white',
+    width: '60%',
+    padding: 5,
+    borderRadius: 5,
+  },
+  login_input: {
+    padding: 5,
+  },
+  login_button: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    margin: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 15,
+    color: 'black',
+    backgroundColor: 'white',
+  },
+});

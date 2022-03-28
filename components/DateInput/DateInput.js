818a5af0171ea18f1c785e47f45d844da8db4e7e
Brynster <b.abendstern@googlemail.com>
2022-03-28 15:34:58 +0100

    create Date-Input-Component

--- components/DateInput/DateInput.js
+++ components/DateInput/DateInput.js
@@ -1,7 +1,9 @@
-import { Text, View, Picker } from 'react-native';
+import { View, Picker } from 'react-native';
 import React, { useState } from 'react';
 
-function DateInput() {
+import styles from './styles';
+
+function DateInput({ setGivenState }) {
   const months = {
     January: 31,
     February: 29,
@@ -17,36 +19,90 @@ function DateInput() {
     December: 31,
   };
 
+  function getYears() {
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    const yearsList = [];
+
+    for (let i = currentYear - 18; i > currentYear - 168; i -= 1) {
+      yearsList.push(String(i));
+    }
+
+    return yearsList;
+  }
+
+  const years = getYears();
+
   const [pickedMonth, setPickedMonth] = useState('Select Month');
   const [daysInPickedMonth, setDaysInPickedMonth] = useState([]);
   const [pickedDay, setPickedDay] = useState('Select Day');
+  const [pickedYear, setPickedYear] = useState('Select Year');
 
   function handlePickMonth(inputMonth) {
     const days = [];
 
+    setPickedMonth(inputMonth);
+
     for (let i = 1; i <= months[inputMonth]; i += 1) {
-      days.push(i);
+      days.push(String(i));
     }
 
     setDaysInPickedMonth(days);
-
-    console.log('days array = ', days);
   }
 
   return (
-    <View>
-      <Picker selectedValue={pickedMonth} onValueChange={handlePickMonth}>
+    <View style={styles.datePicker}>
+      <Picker
+        selectedValue={pickedMonth}
+        onValueChange={(newMonth) => {
+          handlePickMonth(newMonth);
+
+          setGivenState(Date.parse(`${pickedDay} ${newMonth} ${pickedYear}`));
+        }}
+      >
         <Picker.Item label="Select Month" value="Select Month" />
         {Object.keys(months).map((month) => (
-          <Picker.Item key={month} label={month} value={month} />
+          <Picker.Item key={`month-${month}`} label={month} value={month} />
         ))}
       </Picker>
-      <Picker>
+
+      <Picker
+        selectedValue={pickedDay}
+        onValueChange={(newDay) => {
+          setPickedDay(newDay);
+
+          setGivenState(Date.parse(`${newDay} ${pickedMonth} ${pickedYear}`));
+        }}
+      >
         <Picker.Item label="Select Day" value="Select Day" />
-        {months[pickedMonth]}
+        {daysInPickedMonth.map((day) => (
+          <Picker.Item key={`day-${day}`} label={day} value={day} />
+        ))}
+      </Picker>
+
+      <Picker
+        selectedValue={pickedYear}
+        onValueChange={(newYear) => {
+          setPickedYear(newYear);
+
+          setGivenState(Date.parse(`${pickedDay} ${pickedMonth} ${newYear}`));
+        }}
+      >
+        <Picker.Item label="Select Year" value="Select Year" />
+        {years.map((year) => (
+          <Picker.Item key={`year-${year}`} label={year} value={year} />
+        ))}
       </Picker>
     </View>
   );
 }
 
 export default DateInput;
+
+// --== USAGE ==--
+
+// initialise state:
+// const [givenDate, setGivenDate] = useState();
+
+// invoke component, pass setState function on props:
+// <DateInput setGivenState={setGivenDate} />;

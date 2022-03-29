c8bf0b6c61c6e0a57e5496f2ce8fbff1ddfc3e4f
willmcb94 <williammcburney@Williams-Air.fritz.box>
2022-03-29 12:41:04 +0100

    add listed walks screen

--- screens/MyListedWalksScreen/MyListedWalksScreen.js
+++ screens/MyListedWalksScreen/MyListedWalksScreen.js
@@ -4,70 +4,124 @@ import Nav from '../../components/Nav/Nav';
 import UserContext from '../../contexts/UserContext';
 import styles from './styles';
 import { database } from '../../firebase';
-import { onValue, ref } from 'firebase/database';
+import { get, onValue, ref, remove } from 'firebase/database';
 
 function MyListedWalksScreen({ navigation }) {
   const { user } = useContext(UserContext);
   const [walks, setWalks] = useState({});
-
+  const [dogs, setDogs] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const [isLoadingDogs, setIsLoadingDogs] = useState(true);
 
   useEffect(() => {
     onValue(ref(database, `data/walks/${user.uid}`), (res) => {
       setWalks(res.val());
-
       setIsLoading(false);
     });
   }, []);
+  useEffect(() => {
+    onValue(ref(database, `data/dogs/${user.uid}`), (res) => {
+      setDogs(res.val());
+      setIsLoadingDogs(false);
+    });
+  }, []);
 
-  const walksArray = walks !== null ? Object.keys(walks) : [];
-  const walkSections = walksArray.map((walk) => {
-    return {
-      title: walk,
-      data: [walks[walk].walkDesc, walks[walk].walkMinutes, walks[walk].dateTime],
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiLVLnCwenBsArkSI8m3-qSxmFBBgGlooGig&usqp=CAU',
-    };
-  });
+  const handleClickFoundAndDelete = (walkId) => {
+    remove(ref(database, `data/walks/${user.uid}/${walkId}`));
+  };
 
-  if (isLoading)
+  if (isLoading || isLoadingDogs)
     return (
       <View>
         <Text>Loading...</Text>
       </View>
     );
-  if (walkSections.length === 0)
-    return (
-      <View style={styles.no_dog_container}>
-        <Text> You have no dogs added</Text>
 
-        {/* <Button
-            style={styles.addWalk} accessibilityLabel="add-walk-button" onPress={handleAddDog}>
-            Add Dog
-          </Button> */}
-      </View>
+  if (!walks) {
+    return (
+      <>
+        <View style={styles.main_container}>
+          <View style={styles.no_walks_container}>
+            <Text style={styles.no_walks_text}>
+              No walks listed! Please list a walk{' '}
+              <Text
+                style={styles.here_text}
+                onPress={() => {
+                  navigation.navigate('ListAWalkScreen');
+                }}
+              >
+                HERE
+              </Text>
+            </Text>
+          </View>
+        </View>
+        <Nav navigation={navigation} />
+      </>
     );
+  }
+
+  let walksArr = Object.entries(walks);
+
+  walksArr = walksArr.map((walk) => {
+    return walk[0];
+  });
+  console.log(dogs, '<----');
+
   return (
     <>
-      <View style={styles.main_container}>
-        <FlatList
-          data={walkSections}
-          renderItem={({ item }) => (
-            <View>
-              <ScrollView>
-                <Text style={styles.item}>{item.title + ' - ' + item.data[0]}</Text>
-                <Image source={{ uri: item.image }} style={styles.img} />
-                {/* <Button
-                style={styles.edit}
-                onPress={() => { navigation.navigate('SingleDogScreen', { name: item.title, image: item.image, info: item.data }); }}>
-                Edit
-              </Button> */}
-              </ScrollView>
-            </View>
-          )}
-        />
-      </View>
-      <Nav />
+      <ScrollView style={styles.scroll_view}>
+        <View style={styles.main_container}>
+          <View style={styles.header_contain}>
+            <Text style={styles.header}>Listed walks</Text>
+          </View>
+          {walksArr.map((walk) => {
+            return (
+              <View key={walk} style={styles.walk_card}>
+                <View style={styles.img_text}>
+                  <View style={styles.walk_img_contain}>
+                    <Image
+                      style={styles.img}
+                      source={{
+                        uri: dogs[walks[walk].dogId].imageUrl,
+                      }}
+                    />
+                  </View>
+                  <View style={styles.walk_text_contain}>
+                    <Text style={styles.dog_name}>{dogs[walks[walk].dogId].name}</Text>
+                    <Text style={styles.walk_info}>{walks[walk].dateTime}</Text>
+                    <Text style={styles.walk_info}>{`${walks[walk].walkMinutes} minute walk`}</Text>
+                    <Text style={styles.more_info}>More info...</Text>
+                  </View>
+                </View>
+
+                <View style={styles.save_cancel}>
+                  <View style={styles.save_press}>
+                    <Text
+                      style={styles.cancel_save_text}
+                      onPress={() => {
+                        handleClickFoundAndDelete(walk);
+                      }}
+                    >
+                      Walker found
+                    </Text>
+                  </View>
+                  <View style={styles.cancel_press}>
+                    <Text
+                      style={styles.cancel_save_text}
+                      onPress={() => {
+                        handleClickFoundAndDelete(walk);
+                      }}
+                    >
+                      X
+                    </Text>
+                  </View>
+                </View>
+              </View>
+            );
+          })}
+        </View>
+      </ScrollView>
+      <Nav navigation={navigation} />
     </>
   );
 }

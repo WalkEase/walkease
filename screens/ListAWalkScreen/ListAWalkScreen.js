6b9f08c80e1da131eb812d420a05936ea6385dbf
Bartosz <bsarna89@gmail.com>
2022-03-30 09:33:57 +0100

    add dog page ready to push

--- screens/ListAWalkScreen/ListAWalkScreen.js
+++ screens/ListAWalkScreen/ListAWalkScreen.js
@@ -106,7 +106,6 @@ function ListAWalkScreen({ navigation }) {
             }
           })
           .then((dataUpdated) => {
-            console.log(dataUpdated);
             if (dataUpdated !== undefined) navigation.navigate('MyListedWalksScreen');
           })
           .catch((error) => alert(error.message));
